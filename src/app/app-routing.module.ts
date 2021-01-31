5b0353b0293a749b7def7b374a1526903bf27521
Edwin Quintero R <edwi02@gmail.com>
2021-01-31 08:35:57 -0500

    Rutas hijas creadas para el Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,17 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: '', component: DashboardComponent },
+    {
+        path: '',
+        component: DashboardComponent,
+        children: dashboardRoutes
+    },
     { path: '**', redirectTo: '' },
 
 ];
