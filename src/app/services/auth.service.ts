2c2e22e6ae44075fba0a604dbe6ec1ba3e200a20
Edwin Quintero R <edwi02@gmail.com>
2021-01-31 15:14:24 -0500

    Servicio para obtener información del usuario con Firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,6 +8,14 @@ export class AuthService {
 
   constructor( public auth: AngularFireAuth ) { }
 
+  initAuthListener(): void {
+    this.auth.authState.subscribe( fuser => {
+      console.log( fuser );
+      console.log( fuser?.uid );
+      console.log( fuser?.email );
+    });
+  }
+
   crearUsuario(nombre: string, email: string, password: string): Promise<any> {
     return this.auth.createUserWithEmailAndPassword( email, password );
   }
